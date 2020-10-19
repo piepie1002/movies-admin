4aa4cba12c8cdbb8e0a13d6942c4e6ffa6025625
cwg <1113779094@qq.com>
2020-10-19 15:53:52 +0800

    数据库对应的实体类

---
+++ src/main/java/com/tact/movies/entity/Subclass.java
@@ -0,0 +1,47 @@
+package com.tact.movies.entity;
+
+
+public class Subclass {
+
+  private String id;
+  private long isUse;
+  private String name;
+  private String catalogId;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public long getIsUse() {
+    return isUse;
+  }
+
+  public void setIsUse(long isUse) {
+    this.isUse = isUse;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getCatalogId() {
+    return catalogId;
+  }
+
+  public void setCatalogId(String catalogId) {
+    this.catalogId = catalogId;
+  }
+
+}
