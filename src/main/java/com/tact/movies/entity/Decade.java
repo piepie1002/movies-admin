63b4a53062a1d98b7782ec1edcce2a714c866e1e
ArnerW <2367012570@qq.com>
2020-10-21 15:24:28 +0800

    王傲周三午提交123

---
+++ src/main/java/com/tact/movies/entity/Decade.java
@@ -0,0 +1,52 @@
+package com.tact.movies.entity;
+
+
+public class Decade {
+
+  private String id;
+  private long isUse;
+  private String name;
+  private long sort;
+
+  public Decade(String id, long isUse, String name) {
+    this.id = id;
+    this.isUse = isUse;
+    this.name = name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public long getIsUse() {
+    return isUse;
+  }
+
+  public void setIsUse(long isUse) {
+    this.isUse = isUse;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public long getSort() {
+    return sort;
+  }
+
+  public void setSort(long sort) {
+    this.sort = sort;
+  }
+
+}
