8872c76b5ecff296ab2caf089a8817d4867470f5
ArnerW <2367012570@qq.com>
2020-10-21 15:21:54 +0800

    王傲周三午提交12

--- src/main/java/com/tact/movies/entity/Catelog.java
+++ src/main/java/com/tact/movies/entity/Catelog.java
@@ -1,56 +1,60 @@
 package com.tact.movies.entity;
 
 
-<<<<<<< HEAD:src/main/java/com/tact/movies/entity/Decade.java
-public class Decade {
-=======
-public class CateLog {
->>>>>>> cwg:src/main/java/com/tact/movies/entity/CateLog.java
-
-  private String id;
-  private long isUse;
-  private String name;
-  private long sort;
-
-  public Decade(String id, long isUse, String name) {
-    this.id = id;
-    this.isUse = isUse;
-    this.name = name;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-
-  public long getIsUse() {
-    return isUse;
-  }
-
-  public void setIsUse(long isUse) {
-    this.isUse = isUse;
-  }
-
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-
-  public long getSort() {
-    return sort;
-  }
-
-  public void setSort(long sort) {
-    this.sort = sort;
-  }
+  public class CateLog {
+      private String id;
+      private long isUse;
+      private String name;
+      private long sort;
+      private long isVip;
+  
+    public CateLog(String id ,String name) {
+        this.id = id;
+        this.name = name;
+    }
+  
+    public String getId() {
+      return id;
+    }
+  
+    public void setId(String id) {
+      this.id = id;
+    }
+  
+  
+    public long getIsUse() {
+      return isUse;
+    }
+  
+    public void setIsUse(long isUse) {
+      this.isUse = isUse;
+    }
+  
+  
+    public String getName() {
+      return name;
+    }
+  
+    public void setName(String name) {
+      this.name = name;
+    }
+  
+  
+    public long getSort() {
+      return sort;
+    }
+  
+    public void setSort(long sort) {
+      this.sort = sort;
+    }
+  
+  
+    public long getIsVip() {
+      return isVip;
+    }
+  
+    public void setIsVip(long isVip) {
+        this.isVip = isVip;
+    }
 
 }
--- src/main/java/com/tact/movies/service/TypeSelectedService.java
+++
@@ -1,8 +0,0 @@
-package com.tact.movies.service;
-
-import com.tact.movies.data.TypeSelectedData;
-
-public interface TypeSelectedService {
-    TypeSelectedData getSubClassData(String id);
-    TypeSelectedData getTypeData(String id);
-}
